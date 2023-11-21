0954da89198702430698add03150f96750d78f2b
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2023-11-21 20:24:35 -0300

    Cadastro

--- noticia/forms.py
+++ noticia/forms.py
@@ -1,14 +1,25 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, EmailField
 from .models import Autor, Noticia
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
 class AutorForm(ModelForm):
+   
     class Meta:
-        model = Autor
+        model=Autor
         fields='__all__'
-        
+
 class NoticiaForm(ModelForm):
+   
     class Meta:
-        model = Noticia
+        model=Noticia
         fields='__all__'
-        
+
+class RegistrationForm(UserCreationForm):
+
+    email = EmailField(max_length=200, label="Email")
+    
+    class Meta:
+        model = get_user_model()
+        fields=['username', 'email', 'password1', 'password2' ]
         
--- webnews/urls.py
+++ webnews/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from noticia.views import HomeTemplateView
+from noticia.views import HomeTemplateView, RegistrationView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('noticias/', include('noticia.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/registration', RegistrationView.as_view(), name='registration'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
